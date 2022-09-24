b8e73793118bea3ac63c391cee3c7446ccb40aeb
Stian Munkejord <smu027@uib.no>
2022-09-24 17:31:17 +0200

    Init

---
+++ src/ICustomer.java
@@ -0,0 +1,12 @@
+public interface ICustomer {
+
+    /**
+     * @return The name of the customer
+     */
+    public String getName();
+
+    /**
+     * @return if the customer is politically exposed
+     */
+    public boolean isPep();
+}
