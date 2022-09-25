4e7eae1802dc6ec14d89978ceba2c2d0dbb2da7f
Stian Munkejord <smu027@uib.no>
2022-09-25 23:35:05 +0200

    La til bruker/admin lowkey

--- src/KYC_Terminal.java
+++ src/KYC_Terminal.java
@@ -9,9 +9,39 @@ public class KYC_Terminal {
     private static DataBasePerson dataBasePerson = new DataBasePerson();
 
     public static void main(String[] args) {
-
         initDatabase();
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you admin? (y/n)");
+        String input = scanner.nextLine();
+        if (input.toLowerCase().equals("y")) {
+            admin();
+        } else {
+            user();
+        }
+    }
+
+    public static void user() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! Press (c) for list of commands");
+        while (true) {
+
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals("q")) {
+                System.exit(0);
+            } else if (input.toLowerCase().equals("s")) {
+                searchPerson();
+            } else if (input.toLowerCase().equals("p")) {
+                dataBasePerson.printDatabase();
+            } else if (input.toLowerCase().equals("c")) {
+                commands();
+            } else {
+                System.out.println("Command not found. Please try again. Press (c) for commands");
+            }
+        }
+    }
+
+    public static void admin() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome! Press (c) for list of commands");
         while (true) {
@@ -31,6 +61,7 @@ public class KYC_Terminal {
                 System.out.println("Command not found. Please try again. Press (c) for commands");
             }
         }
+
     }
 
     private static void commands() {
@@ -72,27 +103,25 @@ public class KYC_Terminal {
 
         System.out.println("Would you like to give this person an ID? (y/n)");
         String input = scanner.nextLine();
-        if(input.toLowerCase().equals("y")){
+        if (input.toLowerCase().equals("y")) {
             System.out.println("Please write an ID. The ID must be between 0 and " + dataBasePerson.getIDRange());
             input = scanner.nextLine();
-            try{
+            try {
                 int ID = Integer.parseInt(input);
-                if(!dataBasePerson.addNew(ID, newPerson)){
+                if (!dataBasePerson.addNew(ID, newPerson)) {
                     dataBasePerson.addNew(newPerson);
                 }
-            }
-            catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 dataBasePerson.addNew(newPerson);
             }
-        }
-        else{
+        } else {
             dataBasePerson.addNew(newPerson);
         }
 
         System.out.println("Thank you. Optional information WIP");
     }
 
-    private static void searchPerson() {
+    public static void searchPerson() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the ID of the pearson in question. Press (n) if you don't know the ID");
         String input = scanner.nextLine();
@@ -111,7 +140,7 @@ public class KYC_Terminal {
         }
     }
 
-    private static void searchID(int ID){
+    private static void searchID(int ID) {
         Person person = dataBasePerson.getCustomer(ID);
         System.out.println("Printing info: ");
         person.printInfo();
@@ -119,36 +148,34 @@ public class KYC_Terminal {
 
     private static void searchUnknown() {
         Scanner scanner = new Scanner(System.in);
-        while(true) {
+        while (true) {
             System.out.println("Search using: Name (n), birth (b), Country (c). Quit (q)");
             String input = scanner.nextLine();
-            if(input.toLowerCase().equals("q")){
+            if (input.toLowerCase().equals("q")) {
                 break;
             }
-            if(input.toLowerCase().equals("n")){
+            if (input.toLowerCase().equals("n")) {
                 System.out.println("Enter a name you would like to search: ");
                 String name = scanner.nextLine();
                 ArrayList<Person> foundNames = dataBasePerson.nameSearch(name);
                 System.out.println("There where " + foundNames.size() + " results for " + name);
-                for(Person p : foundNames){
+                for (Person p : foundNames) {
                     p.printInfo();
                 }
                 break;
-            }
-            else{
+            } else {
                 System.out.println("Please try again");
             }
         }
 
     }
 
-    private static void initDatabase(){
+    public static void initDatabase() {
         Person stian = new Person("Stian Munkejord", "04-08-2001", "Norway", false);
         dataBasePerson.addNew(1, stian);
         Person erna = new Person("Erna Solberg", "24-02-1961", "Norway", true);
-        dataBasePerson.addNew(2,erna);
+        dataBasePerson.addNew(2, erna);
     }
-
 
 
 }
